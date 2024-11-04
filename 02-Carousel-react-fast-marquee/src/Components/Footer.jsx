c6061222d4282181a02856b491212163e19ead03
Lahiru Kavinda <lahiruxk@gmail.com>
2024-11-04 16:43:18 +0530

    update

--- 02-Carousel-react-fast-marquee/src/Components/Footer.jsx
+++ 02-Carousel-react-fast-marquee/src/Components/Footer.jsx
@@ -1,12 +1,16 @@
-import FooterCarousel from './FooterCarousel';
-import FooterContent from './FooterContent';
+import FooterCarousel from "./FooterCarousel";
+import FooterContent from "./FooterContent";
 
-const Footer = ({ showCarousel = true, showContent = true }) => {
+const Footer = () => {
   return (
-    <footer>
-      {showCarousel && <FooterCarousel />}
-      {showContent && <FooterContent />}
-    </footer>
+    <div className="min-h-screen flex flex-col">
+      <div className="">
+        <FooterCarousel />
+      </div>
+      <div className="bg-[#E6F2FF] flex-grow ">
+        <FooterContent />
+      </div>
+    </div>
   );
 };
 
