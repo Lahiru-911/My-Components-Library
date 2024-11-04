80ee55e56421c881ba4a9aaba5b8cbfa3274694f
Lahiru Kavinda <lahiruxk@gmail.com>
2024-11-04 10:41:38 +0530

    update

--- 02-Carousel-react-fast-marquee/src/App.jsx
+++ 02-Carousel-react-fast-marquee/src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./Components/Footer"
 
 function App() {
  
@@ -5,7 +6,7 @@ function App() {
   return (
     <>
       <div>
-        
+        <Footer/>
       </div>
     </>
   )
---
+++ 02-Carousel-react-fast-marquee/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import FooterCarousel from "./FooterCarousel";
+
+
+const Footer = () => {
+  return (
+    <div>
+        <FooterCarousel/>
+    </div>
+  )
+}
+
+export default Footer;
