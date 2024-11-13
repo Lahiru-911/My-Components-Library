1feaa1378601bba2d5b25ea315ad6e59900d41cc
Lahiru Kavinda <lahiruxk@gmail.com>
2024-11-13 10:11:59 +0530

    update

--- 03-Box-Card-react/src/Card/BoxCard.jsx
+++ 03-Box-Card-react/src/Card/BoxCard.jsx
@@ -2,6 +2,7 @@ const BoxCard = () => {
   return (
     <>
       <div className="grid grid-cols-3 gap-10 m-5">
+        {/* BoxCard01 */}
         <div className="max-w-[500px] max-h-[700px] w-full relative rounded-lg bg-white/60 shadow-2xl ring-1 ring-gray-900/10 transition-transform duration-300 ease-in-out hover:scale-105 z-10">
           <img
             className="rounded-lg"
