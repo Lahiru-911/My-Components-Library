6de941d1abd6bbd64debdd983b19f78482cc780b
Lahiru Kavinda <lahiruxk@gmail.com>
2024-11-13 10:15:00 +0530

    update

--- 03-Box-Card-react/src/Card/BoxCard.jsx
+++ 03-Box-Card-react/src/Card/BoxCard.jsx
@@ -1,6 +1,9 @@
 const BoxCard = () => {
   return (
     <>
+      <h1 className="m-3 p-1 font-bold uppercase text-transparent bg-clip-text bg-gradient-to-r from-[#56CCF2] to-[#0081FB] text-5xl sm:text-5xl md:text-6xl lg:text-8xl xl:text-[120px] 2xl:text-[140px] leading-none">
+        WEB Services
+      </h1>
       <div className="grid grid-cols-3 gap-10 m-5">
         {/* BoxCard01 */}
         <div className="max-w-[500px] max-h-[700px] w-full relative rounded-lg bg-white/60 shadow-2xl ring-1 ring-gray-900/10 transition-transform duration-300 ease-in-out hover:scale-105 z-10">
