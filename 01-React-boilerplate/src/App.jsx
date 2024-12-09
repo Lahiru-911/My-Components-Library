7f3c544c08a45aef39010371dfa3f5ff18a0f3e2
Lahiru Kavinda <lahiruxk@gmail.com>
2024-12-09 21:07:00 +0530

    update

--- 01-React-boilerplate/src/App.jsx
+++ 01-React-boilerplate/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./Routes/Router";
-import Navbar from "./Components/NavBar";
+import NavBar from "./Components/NavBar";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <NavBar />
       <AppRouter />
+      <Footer/>
     </Router>
   );
 }
