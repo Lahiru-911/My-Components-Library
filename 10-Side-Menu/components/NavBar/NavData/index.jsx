fcc8235823e1ac4d2fccfffd5945c338af8c0c23
Lahiru Kavinda <lahiruxk@gmail.com>
2025-03-01 13:31:20 +0530

    update

--- 10-Side-Menu/components/NavBar/NavData/index.jsx
+++ 10-Side-Menu/components/NavBar/NavData/index.jsx
@@ -3,51 +3,48 @@ import { motion } from 'framer-motion';
 import { links, footerLinks } from './data';
 import { perspective, slideIn } from "./anim";
 
-export default function index() {
+export default function Index() {
   return (
     <div className={styles.nav}>
-       <div className={styles.body}>
-        {
-            links.map( (link, i) => {
-                const { title, href } = link;
-                return (
-                    <div key={`b_${i}`} className={styles.linkContainer}>
-                        <motion.div
-                          href={href}
-                          custom={i}
-                          variants={perspective}
-                          initial="initial"
-                          animate="enter"
-                          exit="exit"
-                        >
-                            <a>
-                                {title}
-                            </a>
-                        </motion.div>
-                    </div>
-                )
-            })
-        }
-       </div>
-       <motion.div className={styles.footer}>
-            {
-                footerLinks.map( (link, i) => {
-                    const { title, href } = link;
-                    return (
-                        <motion.a 
-                            variants={slideIn}
-                            custom={i} 
-                            initial="initial"
-                            animate="enter"
-                            exit="exit"
-                            key={`f_${i}`}
-                        >
-                            {title}
-                        </motion.a>
-                    )
-                })
-            }
-       </motion.div>
+      <div className={styles.body}>
+        {links.map((link, i) => {
+          const { title, href } = link;
+          return (
+            <div key={`b_${i}`} className={styles.linkContainer}>
+              <motion.div
+                custom={i}
+                variants={perspective}
+                initial="initial"
+                animate="enter"
+                exit="exit"
+              >
+                <a href={href} className={styles.link}>
+                  {title}
+                </a>
+              </motion.div>
+            </div>
+          );
+        })}
+      </div>
+      <motion.div className={styles.footer}>
+        {footerLinks.map((link, i) => {
+          const { title, href } = link;
+          return (
+            <motion.div
+              variants={slideIn}
+              custom={i}
+              initial="initial"
+              animate="enter"
+              exit="exit"
+              key={`f_${i}`}
+            >
+              <a href={href} className={styles.link}>
+                {title}
+              </a>
+            </motion.div>
+          );
+        })}
+      </motion.div>
     </div>
-  )
+  );
 }
