da025aeacd130148a42cf5314a06d44e4877e097
Lahiru Kavinda <lahiruxk@gmail.com>
2025-03-01 19:08:58 +0530

    update

---
+++ 10-Side-Menu/components/NavBar/NavData/data.js
@@ -0,0 +1,49 @@
+export const links = [
+  {
+    title: "About Me",
+    href: "/",
+  },
+  {
+    title: "Projects",
+    href: "/project",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+  {
+    title: "Expertise",
+    href: "/expertise",
+  },
+  {
+    title: "Experience",
+    href: "/allthree",
+  },
+  {
+    title: "Education",
+    href: "/allthree",
+  },
+  {
+    title: "Blog",
+    href: "/blog",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Facebook",
+    href: "https://www.facebook.com",
+  },
+  {
+    title: "LinkedIn",
+    href: "https://www.linkedin.com",
+  },
+  {
+    title: "Instagram",
+    href: "https://www.instagram.com",
+  },
+  {
+    title: "GitHub",
+    href: "https://github.com",
+  },
+];
