10b6559a8401729076edeb7b0bf460b47359169e
Lahiru Kavinda <lahiruxk@gmail.com>
2025-03-02 16:05:36 +0530

    update

---
+++ 11-3D-Perspective-Text/app/page.js
@@ -0,0 +1,12 @@
+import PerspectiveText from "@/components/PerspectiveText/PerspectiveText";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <PerspectiveText />
+    </div>
+  );
+};
+
+export default Home;
